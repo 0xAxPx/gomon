800c8e316b617a61cd36ebeb5cce7ea3eb11922c
Alex Peshkov <alexander.peshkov@p2p.org>
2025-05-03 21:16:00 +0100

    test actions

--- agent/main.go
+++ agent/main.go
@@ -193,6 +193,7 @@ func main() {
 		}
 		log.Printf("Serialized Metric: %v", data)
 
+		// Send message
 		if err := producer.SendMessage(data); err != nil {
 			log.Printf("Error sending message: %v", err)
 		}
